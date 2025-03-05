7e805ae40e805e9ce81728870541059f8895244e
p-koushik123 <110448355+p-koushik123@users.noreply.github.com>
2025-03-05 10:34:47 +0530

    commit-1

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// You can add interactive JavaScript functionality here if needed
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
